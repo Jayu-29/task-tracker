c3f222b139afaf2f2acf536e044692c266cc0377
Jayu-29 <jaibaradia29@gmail.com>
2026-05-28 09:07:46 +0000

    feat: add environment variable schema validation using zod

---
+++ env.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().min(1),
+  AUTH_SECRET: z.string().min(1),
+  AUTH_GOOGLE_ID: z.string().min(1),
+  AUTH_GOOGLE_SECRET: z.string().min(1),
+  NEXTAUTH_URL: z.string().optional(),
+});
+
+export const env = envSchema.parse(process.env);
